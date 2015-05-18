3a84f5b29c9aa07c8f4155aa8d311d086e453136
Ben Warfield <bwarfield@amplify.com>
2015-05-18 19:42:45 -0400

    Client refactoring, step 2/many: made it an object.
    Moved the basic REST request methods, plus the connection configuration,
    into an object that can be passed around as a single value, then cruelly
    mocked it by just calling the constructor (with fallback to the previous
    ways of setting username/password/host) in most of the specific-action
    methods.

--- stash_client/rest.py
+++ stash_client/rest.py
@@ -60,88 +60,117 @@ def set_creds(args):
     (USERNAME, PASSWORD) = (username, password)
 
 
-def create_url(user=None, project=None, repository=None, api_path=None):
-    if user is not None and project is not None:
-        raise UserError("EITHER user or project may be supplied")
-    components = ['https://%s/rest/api/%s' % (STASH_HOST, STASH_API_VERSION)]
-    if user is not None:
-        components.append(_USER_NAMESPACE)
-        components.append(user)
-    elif project is not None:
-        components.append(_PROJECT_NAMESPACE)
-        components.append(project)
-    if repository is not None:
-        components.append(_REPOSITORY_NAMESPACE)
-        components.append(repository)
-    if api_path is not None:
-        components.extend(api_path)
-    return "/".join(components)
+class StashRestClient(object):
+    """
+    Encapsulate connection logic and host/user/password information in a nice little object.
+    """
+    def __init__(self, host=None, username=None, password=None, api_version=STASH_API_VERSION, dry_run=False):
+        """
+        Set up host/username/password information.  If it is not explicitly passed in, assume fallback to the
+        old-style global configuration variables.
+        """
+        self._host = host or STASH_HOST
+        self._username = username or USERNAME
+        self._password = password or PASSWORD
+        self._api_version = api_version
+        self._dry_run = dry_run
 
+    def _create_url(self, user=None, project=None, repository=None, api_path=None):
+        if user is not None and project is not None:
+            raise UserError("EITHER user or project may be supplied")
+        components = ['https://%s/rest/api/%s' % (self._host, self._api_version)]
+        if user is not None:
+            components.append(_USER_NAMESPACE)
+            components.append(user)
+        elif project is not None:
+            components.append(_PROJECT_NAMESPACE)
+            components.append(project)
+        if repository is not None:
+            components.append(_REPOSITORY_NAMESPACE)
+            components.append(repository)
+        if api_path is not None:
+            components.extend(api_path)
+        return "/".join(components)
 
-def _request(method, user=None, project=None, repository=None, api_path=None,
-             request_body=None, query_params=None):
-    api_url = create_url(user=user, project=project, repository=repository, api_path=api_path)
-    resp = requests.api.request(method, api_url,
-                                auth=(USERNAME, PASSWORD),
-                                data=request_body,
-                                params=query_params,
-                                headers={'Content-type': 'application/json'}
-                                )
-    if not resp.ok:
-        logging.debug("%s request for %s failed with response body %s", method, api_url, resp.text)
-        raise ResponseError(resp)
-    return resp
+    def _request(self, method, user=None, project=None, repository=None, api_path=None,
+                 request_body=None, query_params=None):
+        """
+        Send an arbitrary request to the Stash server, with appropriate credentials and headers.
+        In case of an error, wrap the response in a ResponseError object and raise it.
 
+        If the client was created as a dry-run client, then simply print the method, URL, query parameters
+        and request body, and return (which may cause problems for the caller).
+        """
+        api_url = self._create_url(user=user, project=project, repository=repository, api_path=api_path)
+        if self._dry_run:
+            print "%s %s with query %s and body %s" % (method, api_url, query_params, request_body)
+            return None
+        resp = requests.api.request(method,
+                                    api_url,
+                                    auth=(self._username, self._password),
+                                    data=request_body,
+                                    params=query_params,
+                                    # not actually needed for GET/DELETE?
+                                    headers={'Content-type': 'application/json'}
+                                    )
+        if not resp.ok:
+            logging.debug("%s request for %s failed with response body %s", method, api_url, resp.text)
+            raise ResponseError(resp)
+        return resp
 
-def _send_json_body(method, user=None, project=None, repository=None, api_path=None, post_data=None):
-    if post_data is None:
-        raise Exception("Message body data is not actually allowed to be None")
-    json_string = json.dumps(post_data)
-    logging.debug("%s of %s to %s", method.upper(), json_string, api_path)
-    return _request(method, user, project, repository, api_path, request_body=json_string)
+    def _send_json_body(self, method, user=None, project=None, repository=None, api_path=None,
+                        post_data=None):
+        """
+        Send an arbitrary dictionary object as a JSON string in the body of a PUT or POST request.
+        """
+        if post_data is None:
+            raise Exception("Message body data is not actually allowed to be None")
+        json_string = json.dumps(post_data)
+        logging.debug("%s of %s to %s", method.upper(), json_string, api_path)
+        return self._request(method, user, project, repository, api_path, request_body=json_string)
 
+    def post_json(self, user=None, project=None, repository=None, api_path=None, post_data=None):
+        return self._send_json_body('post', user, project, repository, api_path, post_data)
 
-def post_json(user=None, project=None, repository=None, api_path=None, post_data=None):
-    return _send_json_body('post', user, project, repository, api_path, post_data)
+    def put_json(self, user=None, project=None, repository=None, api_path=None, post_data=None):
+        return self._send_json_body('put', user, project, repository, api_path, post_data)
 
+    def get(self, user=None, project=None, repository=None, api_path=None, query_params=None):
+        logging.debug("Sending GET query params %s to %s", query_params, api_path)
+        return self._request('get', user, project, repository, api_path, query_params=query_params)
 
-def put_json(user=None, project=None, repository=None, api_path=None, post_data=None):
-    return _send_json_body('put', user, project, repository, api_path, post_data)
+    def delete(self, user=None, project=None, repository=None, api_path=None, query_params=None):
+        logging.debug("Sending DELETE query params %s to %s", query_params, api_path)
+        return self._request('delete', user, project, repository, api_path, query_params=query_params)
 
+    def get_paged(self, user=None, project=None, repository=None, api_path=None, query_params=None,
+                  entity_class=None, limit=None, start=None):
+        response_pages = []
+        request_params = {}
+        if query_params:
+            request_params.update(query_params)
 
-def get(user=None, project=None, repository=None, api_path=None, query_params=None):
-    logging.debug("Sending GET query params %s to %s", query_params, api_path)
-    return _request('get', user, project, repository, api_path, query_params=query_params)
+        if start:
+            request_params['start'] = start
+        elif 'start' in request_params:
+            del request_params['start']
 
+        if limit:
+            logging.debug("Using page size limit of %d", limit)
+            request_params['limit'] = limit
+        elif 'limit' in request_params:
+            del request_params['limit']
 
-def get_paged(user=None, project=None, repository=None, api_path=None, query_params=None,
-              entity_class=None, limit=None, start=None):
-    response_pages = []
-    request_params = {}
-    if query_params:
-        request_params.update(query_params)
-
-    if start:
-        request_params['start'] = start
-    elif 'start' in request_params:
-        del request_params['start']
-
-    if limit:
-        logging.debug("Using page size limit of %d", limit)
-        request_params['limit'] = limit
-    elif 'limit' in request_params:
-        del request_params['limit']
-
-    while True:
-        resp = get(user=user, project=project, repository=repository,
-                   query_params=request_params, api_path=api_path)
-        new_page = PagedApiPage(resp.json(), entity_class)
-        response_pages.append(new_page)
-        if new_page.is_last_page:
-            break
-        else:
-            request_params['start'] = new_page.next_page_start
-    return PagedApiResponse(response_pages)
+        while True:
+            resp = self.get(user=user, project=project, repository=repository,
+                            query_params=request_params, api_path=api_path)
+            new_page = PagedApiPage(resp.json(), entity_class)
+            response_pages.append(new_page)
+            if new_page.is_last_page:
+                break
+            else:
+                request_params['start'] = new_page.next_page_start
+        return PagedApiResponse(response_pages)
 
 
 def create_repository(repository_name, user=None, project=None):
@@ -150,13 +179,13 @@ def create_repository(repository_name, user=None, project=None):
     if user is None and project is None:
         raise UserError("new repository needs a project or a user")
     post_data = {'name': repository_name}
-    return post_json(post_data=post_data, user=user, project=project, api_path=[_REPOSITORY_NAMESPACE])
+    return StashRestClient().post_json(post_data=post_data, user=user, project=project, api_path=[_REPOSITORY_NAMESPACE])
 
 
 def list_repositories(user=None, project=None, limit=None):
     if user is None and project is None:
         raise UserError("Repository list needs a project or a user")
-    return get_paged(user, project, api_path=[_REPOSITORY_NAMESPACE], entity_class=StashRepo, limit=limit)
+    return StashRestClient().get_paged(user, project, api_path=[_REPOSITORY_NAMESPACE], entity_class=StashRepo, limit=limit)
 
 
 def list_pull_requests(user=None, project=None, repository=None, state=None):
@@ -169,8 +198,8 @@ def list_pull_requests(user=None, project=None, repository=None, state=None):
     # "withAttributes" (basically count open tasks), "withProperties" (not clear this actually does anything)
     if state is not None:
         query_params['state'] = state
-    return get_paged(user, project, repository, api_path=[_PULL_REQUESTS], query_params=query_params,
-                     entity_class=StashPullRequest)
+    return StashRestClient().get_paged(user, project, repository, api_path=[_PULL_REQUESTS], query_params=query_params,
+                                       entity_class=StashPullRequest)
 
 
 def create_pull_request(pr_data, user=None, project=None, repository=None):
@@ -185,7 +214,7 @@ def create_pull_request(pr_data, user=None, project=None, repository=None):
     #  context: null
     # "exceptionName": "com.atlassian.stash.pull.DuplicatePullRequestException",
     # "existingPullRequest": {pr_object}
-    return post_json(user, project, repository, api_path=[_PULL_REQUESTS], post_data=pr_data)
+    return StashRestClient().post_json(user, project, repository, api_path=[_PULL_REQUESTS], post_data=pr_data)
 
 
 def list_user_permissions(user=None, project=None, filter_on=None):
@@ -200,7 +229,7 @@ def list_permissions(grantee_type, user=None, project=None, filter_on=None):
     if project is None:
         raise UserError("Listing project permissions needs a project")
     post_data = {'filter': filter_on} if filter_on else None
-    resp = get(query_params=post_data, user=user, project=project, api_path=[_PERMISSIONS, grantee_type])
+    resp = StashRestClient().get(query_params=post_data, user=user, project=project, api_path=[_PERMISSIONS, grantee_type])
     if resp.text:
         values = resp.json()["values"]
         for value in values:
@@ -219,8 +248,4 @@ def delete_repository(repository_name, user=None, project=None):
         raise UserError("You must specify a repository")
     if user is None and project is None:
         raise UserError("deleting a repository needs a project or a user")
-    api_url = create_url(user=user, project=project, repository=repository_name)
-    resp = requests.api.delete(api_url, auth=(USERNAME, PASSWORD))
-    if not resp.ok:
-        raise ResponseError(resp)
-    return resp
+    return StashRestClient().delete(user=user, project=project, repository=repository_name)
